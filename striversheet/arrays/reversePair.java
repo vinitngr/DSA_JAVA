0618499d1a8a8a0970bb8172ac5b68cc127abc05
Vinit <vinitnagar56@gmail.com>
2025-07-09 10:17:33 +0530

    reverse Pair Hard

--- striversheet/arrays/reversePair.java
+++ striversheet/arrays/reversePair.java
@@ -3,20 +3,73 @@ package striversheet.arrays;
 import java.util.Arrays;
 
 public class reversePair {
-
-    static int count = 0;
-
+    static int pair = 0;
     public static void main(String[] args) {
-        int[] nums = {1, 3, 2, 3, 1};
-        reversePairs(nums);
-        System.out.println("Reverse pairs count: " + count);
+        int[] arr = {4,1,1};
+        reversePairs(arr);
+        System.out.println(pair);
     }
 
     public static void reversePairs(int[] nums){
-        //todo
+        mergeSort(nums);
     }
 
+    public static int[] mergeSort(int[] nums) {
 
+        if(nums.length == 1){
+            return nums;
+        }
+        int mid = nums.length / 2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(nums , 0 , mid));
+        int[] right = mergeSort(Arrays.copyOfRange(nums , mid , nums.length));
+
+        return merge(left  , right);
+    }
+
+    public static int[] merge(int[] left , int[] right){
+
+        int r = 0;
+        for (int i = 0; i < left.length && r <= right.length; i++) {
+            while(r < right.length){
+                if(left[i] > right[r]*2){
+                    pair = pair + left.length-i ;
+                    r++;
+                } else{
+                    break;
+                }
+            }
+        }
+        int[] mix = new int[left.length + right.length];
+
+        int i =0;
+        int j =0;
+        int k =0;
+
+        while(i< left.length && j < right.length){
+            if (left[i] < right[j]) {
+                mix[k] = left[i];
+                i++;
+            } else {
+                mix[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < left.length){
+            mix[k] = left[i];
+            i++;
+            k++;
+        }
+        while(j < right.length){
+            mix[k] = right[j];
+            j++;
+            k++;
+        }
+
+        return mix;
+    }
 }
 
 
