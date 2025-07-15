9064fc501657b4526c783fb4ed7dad39feb89684
Vinit <vinitnagar56@gmail.com>
2025-07-15 12:58:04 +0530

    easiest : DeleteNode or RemoveNode

---
+++ striversheet/linkedList/DeleteNode.java
@@ -0,0 +1,33 @@
+package striversheet.linkedList;
+
+public class DeleteNode {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+        DeleteNode instance = new DeleteNode();
+
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        instance.deleteNode(node5);
+
+        ListNode curr = node1;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+    }
+}
